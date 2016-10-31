3a48a3c66537eb61c3b5ea2f322789e3dc03d9a4
root <root@webtools.c.crested-surfer-143303.internal>
2016-10-31 05:52:39 +0000

    Implement counter of website visitors.

--- controller.js
+++ controller.js
@@ -1,5 +1,39 @@
 var angMod = angular.module("meapp", ['ngCookies']);
-angMod.controller('mecontroller', function($scope, $http, $cookieStore) {
+angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) {
+    $scope.load = function(){
+	//var date = new Date();
+	//console.log(date);
+	var minkey = $filter('date')(new Date(),'yyyy-MM-dd HH:mm');
+	$http.get("app.php?cmd=incr&key="+minkey)
+		.success(function(){
+			$http.get("app.php?cmd=get&key="+minkey)
+		            .success(function (data) {
+                		// console.log("Get Succeed: ");
+      			        console.log(minkey+" current minute visitors : "+data.data);
+				$scope.timesPerMin=data.data;
+ 			    });
+		});
+	var hourkey = $filter('date')(new Date(),'yyyy-MM-dd HH');
+	$http.get("app.php?cmd=incr&key="+hourkey)
+                .success(function(){
+                        $http.get("app.php?cmd=get&key="+hourkey)
+                            .success(function (data) {
+                                // console.log("Get Succeed: ");
+                                console.log(hourkey+" current Hour visitors : "+data.data);
+                                $scope.timesPerHour=data.data;
+                            });
+                });
+	var daykey = $filter('date')(new Date(),'yyyy-MM-dd');
+	$http.get("app.php?cmd=incr&key="+daykey)
+                .success(function(){
+                        $http.get("app.php?cmd=get&key="+daykey)
+                            .success(function (data) {
+                                // console.log("Get Succeed: ");
+                                console.log(daykey+ " current day visitors : "+data.data);
+                                $scope.timesPerDay=data.data;
+                            });
+                });
+    }
     /*check cookies*/
     $scope.checkcookies = function(){
         console.log("enter function");
@@ -20,16 +54,13 @@ angMod.controller('mecontroller', function($scope, $http, $cookieStore) {
     
     /*sign up page*/
     $scope.signup = function(){
-        angular.forEach($scope.input, function(value, key){
-            console.log(key+" : "+value);
-            $http.get("map.php?cmd=set&key="+key+"&value="+value)
+            $http.get("app.php?cmd=set&key="+$scope.input.email+"&value="+$scope.input.password)
                 .success(function () {
                     $scope.redisResponse = "Updated.";
                      console.log("Set Secceed.");
                     location.href = "login.html";
                 });
             
-        });
 
     }
     
@@ -42,31 +73,23 @@ angMod.controller('mecontroller', function($scope, $http, $cookieStore) {
 
     /* log in page*/
     $scope.login = function(){
-        $http.get("map.php?cmd=get&key=email")
+        $http.get("app.php?cmd=get&key="+$scope.user.email)
             .success(function (data) {
                 // console.log("Get Succeed: ");
                 // console.log("DATA.DATA: "+data.data);
-                 if(data.data == $scope.user.email){
-                    $http.get("map.php?cmd=get&key=password")
-                    .success(function (data2) {
-                        if(data2.data==$scope.user.password){
-                            $cookieStore.put("email", $scope.user.email);
-                            $cookieStore.put("password", $scope.user.password);
-                            location.href = "http://www.info6250.com";
-                        }
-                        else{
-                            alert("Wrong password");
-                            $scope.user.password = "";
-                        }
-                    })
-                }
-                else{
-                    alert("no such user");
-                }
-                
-            })
+                if(data.data==$scope.user.password){
+                      $cookieStore.put("email", $scope.user.email);
+                      $cookieStore.put("password", $scope.user.password);
+                      location.href = "http://www.info6250.com";
+                }else{
+                      alert("Wrong password");
+                      $scope.user.password = "";
+                }                
+	    })
             .error(function () {
-                console.log("Can't get data from redis");
+		alert("no such user");
+		$scope.user.email="";
             });
+    
     }
 });
