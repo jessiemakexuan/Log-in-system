6d8ba7574a6d308aceb47d9df47fac0215b85eb7
root <root@webtools.c.crested-surfer-143303.internal>
2016-11-02 17:34:47 +0000

    delete useless comment

--- controller.js
+++ controller.js
@@ -3,11 +3,8 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
    //hit counter history page 
    $scope.getpassedmin = function(){
         var min = $filter('date')($scope.mintime,'yyyy-MM-dd HH:mm');
-	console.log(min);
 	$http.get("app.php?cmd=get&key="+min)
                             .success(function (data) {
-                                console.log($scope.mintime);
-                                console.log(data.data);
                                 if(data.data ==""){
                                     $scope.minvisitor=0;
                                 }else{            
@@ -18,8 +15,6 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.getpassedhour = function(){
         $http.get("app.php?cmd=get&key="+$scope.hourtime)
                             .success(function (data) {
-                                // console.log("Get Succeed: ");
-                                console.log(data.data);
                                 if(data.data ==""){
                                     $scope.hourvisitor=0;
                                 }else{            
@@ -30,8 +25,6 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.getpassedday = function(){
         $http.get("app.php?cmd=get&key="+$scope.daytime)
                             .success(function (data) {
-                                // console.log("Get Succeed: ");
-                                console.log(data.data);
                                 if(data.data ==""){
                                     $scope.dayvisitor=0;
                                 }else{            
@@ -42,8 +35,6 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.getpassedmonth = function(){
         $http.get("app.php?cmd=get&key="+$scope.monthtime)
                             .success(function (data) {
-                                // console.log("Get Succeed: ");
-                                console.log(data.data);
                                 if(data.data ==""){
                                     $scope.monthvisitor=0;
                                 }else{            
@@ -54,8 +45,6 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.getpassedyear = function(){
         $http.get("app.php?cmd=get&key="+$scope.yeartime)
                             .success(function (data) {
-                                // console.log("Get Succeed: ");
-                                console.log(data.data);
                                 if(data.data ==""){
                                     $scope.yearvisitor=0;
                                 }else{            
@@ -68,64 +57,21 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.load = function(){
         var minkey = $filter('date')(new Date(),'yyyy-MM-dd HH:mm');
         $http.get("app.php?cmd=incr&key="+minkey)
-                .success(function(){
-                       // $http.get("app.php?cmd=get&key="+minkey)
-                            //.success(function (data) {
-                                // console.log("Get Succeed: ");
-                               // console.log(minkey+" current minute visitors : "+data.data);
-                                //$scope.timesPerMin=data.data;
-                           // });
-                });
-//	var lasttime =new Date().setMinutes( (new Date().getMinutes()-1));
-//	var lastminkey = $filter('date')(lasttime,'yyyy-MM-dd HH:mm');
-  //      $http.get("app.php?cmd=get&key="+lastminkey)
-    //            .success(function(data){
-                        // console.log("Get Succeed: ");
-      //                  console.log(lastminkey+" last minute visitors : "+data.data);
-	//		if(data.data == ""){
-	//		    $scope.timesPerLastMin = 0;
-	//		}else{
-          //                  $scope.timesPerLastMin=data.data;
-	//		}
-                            
-          //      });
-        var hourkey = $filter('date')(new Date(),'yyyy-MM-dd HH');
+        
+	var hourkey = $filter('date')(new Date(),'yyyy-MM-dd HH');
         $http.get("app.php?cmd=incr&key="+hourkey)
-                .success(function(){
-       //                 $http.get("app.php?cmd=get&key="+hourkey)
-         //                   .success(function (data) {
-                                // console.log("Get Succeed: ");
-           //                     console.log(hourkey+" current Hour visitors : "+data.data);
-             //                   $scope.timesPerHour=data.data;
-               //             });
-                });
-        var daykey = $filter('date')(new Date(),'yyyy-MM-dd');
+        
+	var daykey = $filter('date')(new Date(),'yyyy-MM-dd');
         $http.get("app.php?cmd=incr&key="+daykey)
-                .success(function(){
-                 //       $http.get("app.php?cmd=get&key="+daykey)
-                   //         .success(function (data) {
-                                // console.log("Get Succeed: ");
-                     //           console.log(daykey+ " current day visitors : "+data.data);
-                       //         $scope.timesPerDay=data.data;
-                         //   });
-                });
-        var monthkey = $filter('date')(new Date(),'yyyy-MM');
+        
+	var monthkey = $filter('date')(new Date(),'yyyy-MM');
         $http.get("app.php?cmd=incr&key="+monthkey)
-                .success(function(){
-                       // $http.get("app.php?cmd=get&key="+monthkey)
-                         //   .success(function (data) {
-                                // console.log("Get Succeed: ");
-                           //     console.log(monthkey+" current Month visitors : "+data.data);
-                             //   $scope.timesPerMonth=data.data;
-                  //          });
-                });
-        var yearkey = $filter('date')(new Date(),'yyyy');
+        
+	var yearkey = $filter('date')(new Date(),'yyyy');
         $http.get("app.php?cmd=incr&key="+yearkey)
                 .success(function(){
                         $http.get("app.php?cmd=get&key="+yearkey)
                             .success(function (data) {
-                                // console.log("Get Succeed: ");
-                                console.log(yearkey+" current Year visitors : "+data.data);
                                 $scope.timesPerYear=data.data;
                             });
                 });
@@ -133,18 +79,15 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     }
     /*check cookies*/
     $scope.checkcookies = function(){
-        console.log("enter function");
         var cookieEmail = $cookieStore.get("email");
         var cookiePassword = $cookieStore.get("password");
         if(cookieEmail!=undefined && cookiePassword!=undefined){
-           console.log("get cookie");
             if(confirm("Hi " + cookieEmail+" , Welcome back! Want to log in directly?(click cancel for new username)") == true){
                 location.href = "http://www.info6250.com/";
             }else{
                 location.href = "signup.html";
             }
         }else{
-            console.log("no cookie");
             location.href = "login.html";
         }
     }
@@ -154,7 +97,6 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
             $http.get("app.php?cmd=set&key="+$scope.input.email+"&value="+$scope.input.password)
                 .success(function () {
                     $scope.redisResponse = "Updated.";
-                     console.log("Set Secceed.");
                     location.href = "login.html";
                 });
             
@@ -172,11 +114,10 @@ angMod.controller('mecontroller', function($scope,$filter, $http, $cookieStore) 
     $scope.login = function(){
         $http.get("app.php?cmd=get&key="+$scope.user.email)
             .success(function (data) {
-                // console.log("Get Succeed: ");
-                // console.log("DATA= "+data);
 		if(data.data == ""){
 			alert("no such user!");
 			$scope.user.email="";
+			$scope.user.password="";
 		}else{
                	    if(data.data==$scope.user.password){
                 	 $cookieStore.put("email", $scope.user.email);
