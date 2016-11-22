fb8310b6ad8d2e6fda477fa1658329daeb4d3a02
Kexuan Ma <ma.kex@husky.neu.edu>
2016-11-21 21:51:37 -0800

    Update app.php

--- app.php
+++ app.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-date_default_timezone_set('America/Chicago');
+date_default_timezone_set('America/Los_Angeles');
 require 'Predis/Autoloader.php';
 use Predis\Collection\Iterator;
 Predis\Autoloader::register();
